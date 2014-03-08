aa839542f5cd7daa441813f59be9c56c6d8fbd9f
Jamie Davies <jamie@jamiedavies.me>
2014-03-08 17:37:57 +0000

    Added package info in __init__.py

--- flow/__init__.py
+++ flow/__init__.py
@@ -1,1 +1,12 @@
-__author__ = 'jamie'
+"""
+    flow
+    ====
+
+    A simple framework that makes it easy to work with data streams in Python.
+
+    :copyright: (c) 2014 by Jamie Davies.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.1'
+__author__ = 'Jamie Davies'
