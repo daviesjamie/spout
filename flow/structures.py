da1b4405d4ce9fb5e28ff0c4dd130a785e1bc2cf
Jamie Davies <jamie@jamiedavies.me>
2014-03-08 16:11:10 +0000

    Made Operations, Predicates and Functions all abstract.

--- flow/structures.py
+++ flow/structures.py
@@ -7,6 +7,8 @@ class Operation(object):
     """
     Defines an operation that can be applied to items in a stream.
     """
+    __metaclass__ = abc.ABCMeta
+
     @abc.abstractmethod
     def perform(self, obj):
         pass
@@ -17,6 +19,8 @@ class Predicate(object):
     Used to apply a boolean test to items in a stream, to determine if it meets some set
     criteria.
     """
+    __metaclass__ = abc.ABCMeta
+
     @abc.abstractmethod
     def test(self, obj):
         pass
@@ -26,6 +30,8 @@ class Function(object):
     """
     Applies a function to items in a stream, producing an appropriate result.
     """
+    __metaclass__ = abc.ABCMeta
+
     @abc.abstractmethod
     def apply(self, input):
         pass
