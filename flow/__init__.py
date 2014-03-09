1d0702e501061d63a672939738b2beb50d471f4c
Jamie Davies <jamie@jamiedavies.me>
2014-03-09 01:41:25 +0000

    Added setup.py, to allow package-isation.

--- flow/__init__.py
+++ flow/__init__.py
@@ -8,5 +8,5 @@
     :license: MIT, see LICENSE for more details.
 """
 
-__version__ = '0.1'
+__version__ = '0.1.0'
 __author__ = 'Jamie Davies'
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="flow",
+      version="0.1.0",
+      description="A simple framework that makes it easy to work with data streams in Python.",
+      url="http://github.com/storborg/funniest",
+      author="Jamie Davies",
+      author_email="jamie@jamiedavies.me",
+      license="MIT",
+      packages=['flow'],
+      zip_safe=False)
