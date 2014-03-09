e7e0ef330226e3064d204514ba1f48ec22ab4aca
Jamie Davies <jamie@jamiedavies.me>
2014-03-09 16:52:06 +0000

    Bumped version number for pip to accept README fix.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="spout",
-    version="0.1.1",
+    version="0.1.2",
     author="Jamie Davies",
     author_email="jamie@jamiedavies.me",
     packages=['spout'],
