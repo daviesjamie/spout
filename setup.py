1291be8599b9be2714004841d79ce64e81e0fd23
Jamie Davies <jamie@jamiedavies.me>
2014-03-09 16:29:32 +0000

    Added longer description (piped straight from README).

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
-setup(name="spout",
-      version="0.1.0",
-      description="A simple framework that makes it easy to work with data streams in Python.",
-      url="http://github.com/daviesjamie/spout",
-      author="Jamie Davies",
-      author_email="jamie@jamiedavies.me",
-      license="MIT",
-      packages=['spout'],
-      zip_safe=False)
+setup(
+    name="spout",
+    version="0.1.0",
+    author="Jamie Davies",
+    author_email="jamie@jamiedavies.me",
+    packages=['spout'],
+    url="http://github.com/daviesjamie/spout",
+    license="MIT",
+    description="A simple framework that makes it easy to work with data streams in Python.",
+    long_description=open('README.md').read(),
+)
