57f04a040584437d929050ad9bc7ef70939e3bbe
Jamie Davies <jamie@jamiedavies.me>
2014-03-09 01:47:33 +0000

    Fixed homepage URL in setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name="flow",
       version="0.1.0",
       description="A simple framework that makes it easy to work with data streams in Python.",
-      url="http://github.com/storborg/funniest",
+      url="http://github.com/daviesjamie/flow",
       author="Jamie Davies",
       author_email="jamie@jamiedavies.me",
       license="MIT",
