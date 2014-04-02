2ae52c21c00bbcca23463ee23369427977904515
Jamie Davies <jamie@jamiedavies.me>
2014-04-02 17:43:31 +0100

    Created operation to output plain text to a file.

--- spout/outputs.py
+++ spout/outputs.py
@@ -8,3 +8,18 @@ class PrintOperation(Operation):
     """
     def perform(self, obj):
         print obj
+
+
+class FileOutputOperation(Operation):
+    """
+    Operation that writes each input item onto a separate line in a file.
+    """
+    def __init__(self, filename):
+        self.output = open(filename, 'w')
+
+    def perform(self, obj):
+        self.output.write(obj)
+
+    def __del__(self):
+        self.output.flush()
+        self.output.close()
